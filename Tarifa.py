d99c4ccc89991a6577d912af3a031c7ae332727c
Tom <ThomasBoyd2013@hotmail.co.uk>
2018-11-19 16:26:55 -0500

    added Tarifa problem

--- Tarifa.py
+++ Tarifa.py
@@ -1,17 +1,7 @@
-import sys
-
-readin = []
-for i in sys.stdin:
-    readin.append(int(i))
-
-X = readin[0]
-monthVals = readin[2:]
-
-dataAvailable = X
-for value in monthVals:
-    dataAvailable += (X - value)
-
-print(dataAvailable)
+x = data = int(input())
+for i in range(int(input())):
+    data += (x - int(input()))
+print(data)
     
 
 
